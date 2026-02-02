cdc042d6c01d28e32b48f61665cd8e86f5bb1381
Zach McMullen <87542111+ZachMcM@users.noreply.github.com>
2026-02-01 21:51:50 -0500

    fixing notification url issue

--- server/src/utils/pushNotifications.ts
+++ server/src/utils/pushNotifications.ts
@@ -10,7 +10,7 @@ export async function sendPushNotifications({
   userIds,
   title,
   body,
-  data = { url: "/activity" },
+  data,
 }: {
   userIds: string[];
   title: string;
@@ -76,7 +76,7 @@ export async function sendPushNotification({
   userId,
   title,
   body,
-  data = { url: "/activity" },
+  data,
 }: {
   userId: string;
   title: string;
